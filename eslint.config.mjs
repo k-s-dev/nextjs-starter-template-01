a75354466df558b764e3ddedb022fbe78df78365
k-s-dev <kushagra.singh@gmail.com>
2025-10-13 17:51:53 +0530

    setup: prisma safeql eslint integration for raw queries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,44 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import { globalIgnores } from "@eslint/config-helpers";
+import nextEnv from "@next/env";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const env = nextEnv.loadEnvConfig(__dirname, true);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   globalIgnores(["src/generated/**/*"]),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    plugins: ["@ts-safeql/eslint-plugin"],
+    parserOptions: {
+      project: "./tsconfig.json",
+    },
+    rules: {
+      "@ts-safeql/check-sql": [
+        "error",
+        {
+          connections: [
+            {
+              connectionUrl: env.combinedEnv.DATABASE_URL,
+              migrationsDir: "./src/database/migrations",
+              targets: [
+                {
+                  tag: "prisma.+($queryRaw|$executeRaw)",
+                  transform: "{type}[]",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
