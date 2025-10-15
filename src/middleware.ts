c260e7e42f7040e8c7f8f4a9c70bdf2fd3733306
k-s-dev <kushagra.singh@gmail.com>
2025-10-15 15:22:54 +0530

    migrate:better-auth: remove authjs setup (breaking)
    - fix broken auth related functionality later

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,49 +1,10 @@
-import { NextResponse } from "next/server";
-import { routes } from "@/lib/utils/routeMapper";
-import { authentication } from "./lib/features/authentication/config";
+import { NextRequest, NextResponse } from "next/server";
 
-export default authentication((req) => {
-  const isLoggedIn = !!req.auth;
-  const isAuthAPIRoute = req.nextUrl.pathname.startsWith(routes.authAPIPrefix);
-  const isTestAPIRoute = Object.values(routes.api.test).includes(
-    req.nextUrl.pathname,
-  );
-  const isPublicRoute = routes.public.some((route) => {
-    return typeof route === "string" && route === req.nextUrl.pathname;
-  });
-  const isAuthenticationRoute = Object.values(routes.authentication).includes(
-    req.nextUrl.pathname,
-  );
-
-  if (isAuthAPIRoute || isTestAPIRoute) return;
-
-  if (!isLoggedIn) {
-    if (isAuthenticationRoute) return;
-
-    if (!isPublicRoute) {
-      const redirectUrl = new URL(
-        routes.authentication.signIn,
-        req.nextUrl.origin,
-      );
-      const response = NextResponse.redirect(redirectUrl);
-      return response;
-    }
-  }
-
-  if (isLoggedIn) {
-    if (req.nextUrl.pathname === routes.authentication.resetPassword) return;
-    if (isAuthenticationRoute) {
-      const redirectUrl = new URL(
-        routes.DEFAULT_LOGIN_REDIRECT,
-        req.nextUrl.origin,
-      );
-      const response = NextResponse.redirect(redirectUrl);
-      return response;
-    }
-  }
-
-  return;
-});
+// TODO: review eslint disable
+// eslint-disable-next-line
+export default function middleware(request: NextRequest) {
+  return NextResponse.next()
+}
 
 // Read more: https://clerk.com/docs/quickstarts/nextjs#add-clerk-middleware-to-your-app
 // route matcher to include/exclude middleware on certain paths
